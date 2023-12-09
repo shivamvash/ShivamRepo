b929916086e2be5f077ec946f3170e90b4e920b8
Shivam Vashishth <shivam.vashishth@cloudsufi.com>
2023-12-09 15:20:34 +0530

    Sum of Two Numbers:

---
+++ src/SumTwoNumbers.java
@@ -0,0 +1,9 @@
+public class SumTwoNumbers {
+    public static void main(String[] args) {
+        int a=3;
+        int b=2;
+        int sum=a+b;
+        System.out.println("The Sum of Two Numbers is: "+ sum);
+    }
+}
+
