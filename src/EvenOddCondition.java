b1c68d6ad3e676302f555dfc5a952ce62e522bb8
Shivam Vashishth <shivam.vashishth@cloudsufi.com>
2023-12-09 15:20:34 +0530

    Evon_Odd_Number_Condition(if else)

---
+++ src/EvenOddCondition.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+public class EvenOddCondition {
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+
+        System.out.println("Enter the Number : ");
+        Float Num= sc.nextFloat();
+
+        if (Num%2==0){
+            System.out.println("Even Number");
+        }
+        else {
+            System.out.println("Odd Number");
+        }
+    }
+}
