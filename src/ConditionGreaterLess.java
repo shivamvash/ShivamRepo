3473950c7d8666e1ee1aebb5960da0a86415b3e9
Shivam Vashishth <shivam.vashishth@cloudsufi.com>
2023-12-10 17:29:26 +0530

    Condition_Greater_Less_Using_Two_Numbers_By_Taking_User_Input

---
+++ src/ConditionGreaterLess.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class ConditionGreaterLess {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner (System.in);
+
+        System.out.println("Enter The First Number");
+        float Num1= sc.nextFloat();
+
+        System.out.println("Enter The Second Number");
+        float Num2= sc.nextFloat();
+
+        if (Num1==Num2){
+            System.out.println("Both Numbers are Equal Number");
+        }
+            else {
+                if (Num1>Num2){
+                    System.out.println("First Number is Greater");
+                }
+                    else {
+                    System.out.println("First Number is Less");
+            }
+
+        }
+    }
+}
